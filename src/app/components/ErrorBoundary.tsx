81a62a73ef7e63ade6f7b26550abe952430e1c53
ruzkhanhazar <ruzkhanhazar@gmail.com>
2026-04-07 05:54:00 +0530

    Make Try Again button reload the page instead of resetting error state

--- src/app/components/ErrorBoundary.tsx
+++ src/app/components/ErrorBoundary.tsx
@@ -25,7 +25,7 @@ export class ErrorBoundary extends Component<Props, State> {
   }
 
   handleRetry = () => {
-    this.setState({ hasError: false, error: undefined });
+    window.location.reload();
   };
 
   render() {
