ab49764e488183724f06aab98ad6498cb5661268
Ruzkhan <ruzkhanhazar@gmail.com>
2026-05-12 14:09:33 +0530

    Fix scroll animation triggering too late - add rootMargin for earlier trigger

--- src/app/components/ScrollAnimation.tsx
+++ src/app/components/ScrollAnimation.tsx
@@ -39,7 +39,7 @@ export default function ScrollAnimation({
           element.style.transform = getInitialTransform(animation);
         }
       },
-      { threshold: 0.1 }
+      { threshold: 0, rootMargin: "0px 0px 80px 0px" }
     );
 
     observer.observe(element);
